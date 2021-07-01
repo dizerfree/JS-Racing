a154140055dd48b71228070a859de9f19a593a4c
nataliia <dizerfree@gmail.com>
2021-07-01 22:29:41 +0300

    add logic for moving car

--- main.js
+++ main.js
@@ -27,12 +27,33 @@ function startGame() {
     start.classList.add('hide');
     setting.start = true;
     gameArea.appendChild(car);
+    setting.x = car.offsetLeft;
+    setting.y = car.offsetTop;
     requestAnimationFrame(playGame);
 }
 
 function playGame() {
     console.log('Play game!');
     if (setting.start) {
+        if (keys.ArrowLeft && setting.x > 0) {
+            setting.x -= setting.speed;
+        }
+
+        if (keys.ArrowRight && setting.x < (gameArea.offsetWidth - car.offsetWidth)) {
+            setting.x += setting.speed;
+        }
+
+        if (keys.ArrowUp && setting.y > 0) {
+            setting.y -= setting.speed;
+        }
+
+        if (keys.ArrowDown && setting.y < (gameArea.offsetHeight - car.offsetHeight)) {
+            setting.y += setting.speed;
+        }
+
+        car.style.left = setting.x + 'px';
+        car.style.top = setting.y + 'px';
+
         requestAnimationFrame(playGame);
     }
 }
